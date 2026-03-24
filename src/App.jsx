5b6d24261ed390fa3af2da619c4a2b612bd7bb09
msn0202 <msnblr0202@gmail.com>
2026-03-25 02:20:00 +0530

    second code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-     <h1>MANJUNATH</h1>
+      <h1>MANJUNATH</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
